8eba415e59c8204907e1a99af2cfaab5e3e9525f
Mihailo Vlajkovic <61310737+idakucamupeva@users.noreply.github.com>
2020-11-15 03:01:23 +0100

    image is now displayed when uploaded

--- web/signup/js/global.js
+++ web/signup/js/global.js
@@ -68,3 +68,16 @@
     
 
 })(jQuery);
+
+function showPreview(event){
+  if(event.target.files.length > 0){
+    var src = URL.createObjectURL(event.target.files[0]);
+    var preview = document.getElementById("file-ip-1-preview");
+    preview.src = src;
+    preview.width = 90;
+    preview.height = 90;
+    //preview.style.display = "block";
+    document.getElementById("nesto").style.display= 'none';
+
+  }
+}
