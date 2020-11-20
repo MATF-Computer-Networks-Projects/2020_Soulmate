8a0fcf1d4ad8b34d1e4fe21a5d83ce3655600b2d
djordje <djordje.tanaskovic1998@gmail.com>
2020-11-20 09:50:30 +0100

    Converted to SSL Server
    Not working, problem with certificate

--- src/server/GlobalData.java
+++ src/server/GlobalData.java
@@ -7,6 +7,11 @@ public class GlobalData {
     public static final String DEFAULT_FILE = "signup/index.html";
     public static final String FILE_NOT_FOUND = "errors/404.html";
     public static final String METHOD_NOT_SUPPORTED = "errors/not_supported.html";
-    public static final int PORT = 8080;
+    public static final int PORT = 5252;
     public static final boolean verbose = true;  // Debug switch
+
+    public static final String KS_PATH = "clientkeystore";
+    public static final String KS_PASS = "soulmate";
+    public static final String TS_PATH = "myTrustStore";
+    public static final String TS_PASS = "soulmate";
 }
--- src/server/core/Server.java
+++ src/server/core/Server.java
@@ -1,8 +1,13 @@
 package server.core;
 
+import javax.net.ssl.*;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Objects;
 
@@ -19,7 +24,37 @@ public class Server implements Runnable{
 
     @SuppressWarnings("InfiniteLoopStatement")
     public static void startServer() throws IOException {
-        ServerSocket ss = new ServerSocket(PORT);
+
+        System.setProperty("javax.net.ssl.keyStore", KS_PATH);
+        System.setProperty("javax.net.ssl.keyStorePassword", KS_PASS);
+        System.setProperty("javax.net.ssl.trustStore", TS_PATH);
+        System.setProperty("javax.net.ssl.trustStorePassword", TS_PASS);
+
+        SSLServerSocketFactory factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        SSLServerSocket ss = (SSLServerSocket) factory.createServerSocket(PORT);
+        ss.setNeedClientAuth(true);
+
+
+        String[] supported = ss.getSupportedCipherSuites();
+        System.out.println(Arrays.toString(supported));
+        // Then get all those which don't require authentication (_anon_)
+        String[] anonCipherSuitesSupported = new String[supported.length];
+        int numAnonCipherSuitesSupported = 0;
+        for (String s : supported) {
+            if (s.indexOf("_anon_") > 0)
+                anonCipherSuitesSupported[numAnonCipherSuitesSupported++] = s;
+        }
+
+        // Combine
+        String[] oldEnabled = ss.getEnabledCipherSuites();
+        String[] newEnabled = new String[oldEnabled.length + numAnonCipherSuitesSupported];
+        System.arraycopy(oldEnabled, 0, newEnabled, 0, oldEnabled.length);
+        System.arraycopy(anonCipherSuitesSupported, 0, newEnabled, oldEnabled.length, numAnonCipherSuitesSupported);
+
+        ss.setEnabledCipherSuites(newEnabled);
+
+
+        //ServerSocket ss = new ServerSocket(PORT);
         System.out.println("Listening on port:" + PORT);
 
         while(true) {
@@ -38,6 +73,8 @@ public class Server implements Runnable{
     @Override
     public void run() {
 
+        System.out.println("connected - " + socket.getRemoteSocketAddress());
+
         BufferedReader in = null;
         PrintWriter out = null;
         BufferedOutputStream dataOut = null;
@@ -45,7 +82,7 @@ public class Server implements Runnable{
 
         try {
 
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
             out = new PrintWriter(socket.getOutputStream());
             dataOut = new BufferedOutputStream(socket.getOutputStream());
             Mediator mediator = new Mediator(in, out, dataOut);
@@ -74,10 +111,6 @@ public class Server implements Runnable{
         } finally { // Closing opened streams
 
             try {
-
-                Objects.requireNonNull(in).close();
-                Objects.requireNonNull(out).close();
-                Objects.requireNonNull(dataOut).close();
                 socket.close();
 
             } catch (Exception e) {
@@ -85,7 +118,7 @@ public class Server implements Runnable{
             }
 
             if (verbose) {
-                System.out.println("Connection closed.\n");
+                System.out.println("Connection closed - " + socket.getRemoteSocketAddress() + ".\n");
             }
         }
     }
